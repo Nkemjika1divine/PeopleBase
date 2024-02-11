b39f444715c0f1777c254fbe796cfc76a811387e
Nkemjika1divine <cndivine@gmail.com>
2024-02-11 21:01:28 +0100

    constructor updated to accept key value pairs

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 <h3>Functionalities:</h3>
 <b>.save():</b> This method updates the object to it's current status.
+<b>.to_dict():</b> This returns a dictionry representation of the object. It returns the id, __class__, time_created and time_updated
--- models/basemodel.py
+++ models/basemodel.py
@@ -3,10 +3,17 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.time_created = datetime.now()
-        self.time_updated = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'time_created' or key == 'time_updated':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+        else:
+            self.id = str(uuid4())
+            self.time_created = datetime.now()
+            self.time_updated = datetime.now()
     
     def __str__(self):
         print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
