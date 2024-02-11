b0ae8aef3a00e3a772a5224d43ab191f4465c6cc
Nkemjika1divine <cndivine@gmail.com>
2024-02-11 12:01:18 +0100

    save method and todict method created

--- models/basemodel.py
+++ models/basemodel.py
@@ -4,13 +4,17 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self):
-        self.id = uuid4()
+        self.id = str(uuid4())
         self.time_created = datetime.now()
         self.time_updated = datetime.now()
+    
+    def __str__(self):
+        print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
-    def all(self):
+    """def all(self):"""
     
     def to_dict(self):
+        
 
     def save(self):
-        
+        self.time_updated = datetime.now()
---
+++ testfile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.basemodel import BaseModel
+from models.person import Person
+
+if __name__ == "__main__":
+    user = Person()
+    user.__str__()
+    print(user.time_updated)
+    user.save()
+    print(user.time_updated)
