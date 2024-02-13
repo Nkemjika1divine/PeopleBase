f8ec4d4a2d5af76cd73931ad45dbf539f38fd505
Nkemjika1divine <cndivine@gmail.com>
2024-02-13 14:28:08 +0100

    Fixing serialization and deserialization

--- models/basemodel.py
+++ models/basemodel.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
-import models
+import models.__init__
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
@@ -11,6 +11,8 @@ class BaseModel:
                     continue
                 elif key == 'time_created' or key == 'time_updated':
                     setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.time_created = datetime.now()
@@ -18,15 +20,18 @@ class BaseModel:
             models.storage.new(self)
     
     def __str__(self):
-        print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def to_dict(self):
         copy = self.__dict__.copy()
         copy['__class__'] = self.__class__.__name__
-        copy['time_created'] = self.time_created.isoformat()
-        copy['time_updated'] = self.time_updated.isoformat()
+        if 'time_created' in copy:
+            copy['time_created'] = self.time_created.isoformat()
+        if 'time_updated' in copy:
+            copy['time_updated'] = self.time_updated.isoformat()
         return copy
 
     def save(self):
         self.time_updated = datetime.now()
         models.storage.new(self)
+        models.storage.save()
