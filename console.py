a6440ee7ffa3f89b6cd9ad1e06da6a0ff39f22a1
Nkemjika1divine <cndivine@gmail.com>
2024-02-16 08:04:07 +0100

    help in the console

--- console.py
+++ console.py
@@ -2,7 +2,22 @@
 import cmd
 
 class PeopleBase(cmd.Cmd):
-    pass
+    prompt = "PeopleBase: "
+
+    def do_quit(self, arg):
+        return True
+    
+    def do_exit(self, arg):
+        return True
+    
+    def do_EOF(self, arg):
+        return True
+    
+    def help_exit(self):
+        print("'exit' exits the program")
+    
+    def help_quit(self):
+        print("'quit' exits the program")
 
 if __name__ == '__main__':
     PeopleBase().cmdloop()
