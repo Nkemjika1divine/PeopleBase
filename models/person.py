54f35a796a3273c868ce35470372104bde184259
Nkemjika1divine <cndivine@gmail.com>
2024-02-26 14:35:30 +0100

    Search function implemented but image search isnt implemented yet

--- models/person.py
+++ models/person.py
@@ -26,23 +26,46 @@ class Person(BaseModel, Base):
         marital_status = ""
     elif os.environ.get("PEOPLEBASE_STORAGE_TYPE") == "db":
         __tablename__ = "persons"
-        id = Column(Integer, primary_key=True)
-        first_name = Column(String(20))
+        id = Column(Integer, primary_key=True, nullable=False)
+        first_name = Column(String(20), nullable=False)
         middle_name = Column(String(20))
-        last_name = Column(String(20))
-        date_of_birth = Column(Date)
-        gender = Column(String(10))
-        address = Column(String(150))
-        city = Column(String(20))
-        state = Column(String(20))
-        country = Column(String(20))
-        phone_number = Column(String(20))
-        email = Column(String(60))
-        nationality = Column(String(20))
-        occupation = Column(String(20))
-        education_level = Column(String(20))
-        marital_status = Column(String(20))
+        last_name = Column(String(20), nullable=False)
+        photo = Column(String(300), nullable=False)
+        photo_type = Column(String(30), nullable=False)
+        date_of_birth = Column(Date, nullable=False)
+        gender = Column(String(10), nullable=False)
+        address = Column(String(150), nullable=False)
+        city = Column(String(20), nullable=False)
+        state = Column(String(20), nullable=False)
+        country = Column(String(20), nullable=False)
+        phone_number = Column(String(20), nullable=False)
+        email = Column(String(60), nullable=False)
+        nationality = Column(String(20), nullable=False)
+        occupation = Column(String(20), nullable=False)
+        education_level = Column(String(20), nullable=False)
+        marital_status = Column(String(20), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """initialize person"""
         super().__init__(*args, **kwargs)
+
+def search(line=None):
+    from models.engine.dbstorage import DBStorage
+    from models.__init__ import storage
+
+    if line:
+        similar_objects = {}
+        all_objects = DBStorage.all()
+        if os.path.isfile(line):
+            for obj in all_objects:
+                similarity = check_image(obj.photo, line)
+                if similarity == True:
+                    obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                    similar_objects[obj_key] = obj
+            return True
+        else:
+            for obj in all_objects:
+                if obj.phone_number == line or obj.email == line:
+                    obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+                    similar_objects[obj_key] = obj
+            return True
